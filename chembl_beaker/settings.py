5ae81c1583603f76166a857d4e28438afae962f2
Michał Nowotka <mmmnow@gmail.com>
2013-12-05 19:49:18 +0100

    Update settings.py

--- chembl_beaker/settings.py
+++ chembl_beaker/settings.py
@@ -4,4 +4,4 @@ DEBUG = True
 BOTTLE_PORT = 8079
 BOTTLE_HOST = 'localhost'
 SERVER_MIDDLEWARE = 'tornado'
-OSRA_BINARIES_LOCATION = {'1.4.0': '/usr/local/bin/osra', '2.0.0':'/home/mnowotka/Downloads/osra-2.0.0/src/osra'}
+OSRA_BINARIES_LOCATION = {'2.0.0': '/usr/bin/osra'}
