5e292dea0697c5680dad2cb83a1fde263629f222
Michał Nowotka <mmmnow@gmail.com>
2013-12-19 18:11:03 +0000

    version bump

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if sys.version_info < (2, 7, 3) or sys.version_info >= (2, 7, 6):
 
 setup(
     name='chembl_beaker',
-    version='0.1.1',
+    version='0.1.3',
     author='Michal Nowotka',
     author_email='mnowotka@ebi.ac.uk',
     description='RDKit in the Bottle on Tornado',
