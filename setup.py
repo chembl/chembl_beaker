530c80bf864c8b95a9025cde25befd4e1cdeccdb
Michał Nowotka <mmmnow@gmail.com>
2014-01-08 16:05:24 +0000

    version bump

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if sys.version_info < (2, 7, 3) or sys.version_info >= (2, 7, 6):
 
 setup(
     name='chembl_beaker',
-    version='0.2.0',
+    version='0.2.1',
     scripts=['run_beaker.py'],
     author='Michal Nowotka',
     author_email='mnowotka@ebi.ac.uk',
