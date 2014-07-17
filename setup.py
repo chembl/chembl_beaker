9c7722d7f147da900514d905615b773187b2e708
Michal Nowotka <mnowotka@ebi.ac.uk>
2014-07-17 11:11:58 +0100

    changing absolute paths to relative in documentation

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
 
 setup(
     name='chembl_beaker',
-    version='0.5.4',
+    version='0.5.5',
     entry_points={
         'console_scripts': [
             'run_beaker=chembl_beaker.run_beaker:main']
