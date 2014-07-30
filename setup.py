8eff79a88c8e56ee89d7331298abe29317c0ebf1
Michal Nowotka <mnowotka@ebi.ac.uk>
2014-07-30 14:04:02 +0100

    one more static file bugfix

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
 
 setup(
     name='chembl_beaker',
-    version='0.5.6',
+    version='0.5.7',
     entry_points={
         'console_scripts': [
             'run_beaker=chembl_beaker.run_beaker:main']
