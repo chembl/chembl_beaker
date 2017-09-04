1dbc13f794bd07be98bb276404c847ad6e6ccc47
mnowotka <mmmnow@gmail.com>
2017-09-04 11:11:49 +0100

    version bump

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
 
 setup(
     name='chembl-beaker',
-    version='1.2.2',
+    version='1.2.3',
     entry_points={
         'console_scripts': [
             'run_beaker=chembl_beaker.run_beaker:main']
