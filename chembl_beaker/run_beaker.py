426eb5ceeb55de20c15faf5a778b55cae1ad4108
mnowotka <mmmnow@gmail.com>
2017-09-08 16:29:11 +0100

    main function of run_beaker refactored

--- chembl_beaker/run_beaker.py
+++ chembl_beaker/run_beaker.py
@@ -12,23 +12,25 @@ from chembl_beaker.beaker import app, config, loadPlugins, loadApps
 # ----------------------------------------------------------------------------------------------------------------------
 
 
-parser = OptionParser()
-parser.add_option("--beaker_config", dest="config_path", help="path to config file", default="beaker.conf")
-(options, args) = parser.parse_args()
-conf_path = options.config_path
+def main(conf_path=None):
 
-config.load_config(conf_path)
+    standalone = False
 
-apps = json.loads(config.get('installed_apps', '[]'))
-plugins = json.loads(config.get('plugins', '[]'))
+    if conf_path:
+        config.load_config(conf_path)
+    else:
+        standalone = True
+        parser = OptionParser()
+        parser.add_option("-c", "--config", dest="config_path", help="path to config file", default="beaker.conf")
+        (options, args) = parser.parse_args()
+        conf_path = options.config_path
+        config.load_config(conf_path)
 
-loadApps(apps)
-loadPlugins(app, plugins)
+    apps = json.loads(config.get('installed_apps', '[]'))
+    plugins = json.loads(config.get('plugins', '[]'))
 
-# ----------------------------------------------------------------------------------------------------------------------
-
-
-def main():
+    loadApps(apps)
+    loadPlugins(app, plugins)
 
     server = config.get('server_middleware', 'tornado')
     kwargs = {}
@@ -39,8 +41,11 @@ def main():
             print e
             kwargs['workers'] = 4
 
-    run(app=app, host=config.get('bottle_host', 'localhost'), port=config.get('bottle_port', '8080'),
-        debug=config.get('debug', True), server=server, **kwargs)
+    if standalone:
+        run(app=app, host=config.get('bottle_host', 'localhost'), port=config.get('bottle_port', '8080'),
+            debug=config.get('debug', True), server=server, **kwargs)
+    else:
+        return app
 
 if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
 
 setup(
     name='chembl-beaker',
-    version='1.2.9',
+    version='1.2.10',
     entry_points={
         'console_scripts': [
             'run_beaker=chembl_beaker.run_beaker:main']
