df3f11bbc87216c238d46d0f4013e44783a1279d
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2019-04-18 14:59:03 +0100

    new version number

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
 
 setup(
     name='chembl-beaker',
-    version='1.2.21',
+    version='1.2.22',
     entry_points={
         'console_scripts': [
             'run_beaker=chembl_beaker.run_beaker:main']
