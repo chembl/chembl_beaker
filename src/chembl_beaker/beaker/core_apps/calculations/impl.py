df55705dd8e091e5a69e1fd8b69f4a144da56d4c
Eloy Felix <eloyfelix@gmail.com>
2020-07-23 18:20:15 +0100

    update RemoveHs

--- src/chembl_beaker/beaker/core_apps/calculations/impl.py
+++ src/chembl_beaker/beaker/core_apps/calculations/impl.py
@@ -1,18 +1,21 @@
-import beaker.utils.chemical_transformation as ct
 from beaker.utils.functional import _apply
 from beaker.utils.io import _parseMolData, _getSDFString
-
-# ----------------------------------------------------------------------------------------------------------------------
-
-def _addHs(data, explicitOnly=False, addCoords=False):
-    mols = _parseMolData(data, loadMol=True, useRDKitChemistry=True)
-    mols = _apply(mols, ct._addHs, explicitOnly=explicitOnly, addCoords=addCoords)
-    return _getSDFString(mols)
+from beaker.core_apps.D2Coords.impl import _check3Dcoords
+from chembl_structure_pipeline.standardizer import remove_hs_from_mol
+from rdkit import Chem
 
 # ----------------------------------------------------------------------------------------------------------------------
 
 
-def _removeHs(data, implicitOnly=False):
+def _removeHs(data):
     mols = _parseMolData(data, loadMol=True, useRDKitChemistry=False)
-    mols = _apply(mols, ct._removeHs, implicitOnly=implicitOnly)
+    for mol in mols:
+        Chem.FastFindRings(mol)
+        if mol.NeedsUpdatePropertyCache():
+            mol.UpdatePropertyCache(strict=False)
+        if _check3Dcoords(mol):
+            Chem.AssignStereochemistryFrom3D(mol)
+        else:
+            Chem.AssignAtomChiralTagsFromStructure(mol)
+    mols = _apply(mols, remove_hs_from_mol)
     return _getSDFString(mols)
--- src/chembl_beaker/beaker/core_apps/calculations/views.py
+++ src/chembl_beaker/beaker/core_apps/calculations/views.py
@@ -1,43 +1,13 @@
 from beaker import app
-from bottle import request, response
-from beaker.core_apps.calculations.impl import  _addHs, _removeHs
-from beaker.utils.io import _parseFlag
+from bottle import request
+from beaker.core_apps.calculations.impl import _removeHs
 
-# ----------------------------------------------------------------------------------------------------------------------
-
-
-def addHsView(data, params):
-
-    kwargs = dict()
-    kwargs['explicitOnly'] = _parseFlag(params.get('explicitOnly', False))
-    kwargs['addCoords'] = _parseFlag(params.get('addCoords', False))
-
-    return _addHs(data, **kwargs)
-
-# ----------------------------------------------------------------------------------------------------------------------
-
-
-@app.route('/addHs', method=['OPTIONS', 'POST'], name="addHs")
-def addHs():
-    """
-Adds hydrogens to the graph of a molecule. CTAB is either single molfile or SDF file.
-cURL examples:
-
-    curl -X POST --data-binary @addHs.mol ${BEAKER_ROOT_URL}addHs
-    curl -X POST -F "file=@addHs.mol" -F "addCoords=1" ${BEAKER_ROOT_URL}addHs
-    """
-
-    data = request.files.values()[0].file.read() if len(request.files) else request.body.read()
-    return addHsView(data, request.params)
 
 # ----------------------------------------------------------------------------------------------------------------------
 
 
 def removeHsView(data, params):
-
-    kwargs = dict()
-    kwargs['implicitOnly'] = _parseFlag(params.get('implicitOnly', False))
-    return _removeHs(data, **kwargs)
+    return _removeHs(data)
 
 # ----------------------------------------------------------------------------------------------------------------------
 
