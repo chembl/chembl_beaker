9c0be9eae742a1ea4c8ed70b4891bc02a3022f3e
Eloy Felix <eloyfelix@gmail.com>
2020-07-24 16:44:47 +0100

    add props to removehs mols

--- src/chembl_beaker/beaker/core_apps/calculations/impl.py
+++ src/chembl_beaker/beaker/core_apps/calculations/impl.py
@@ -1,15 +1,33 @@
-from beaker.utils.functional import _apply
-from beaker.utils.io import _parseMolData, _getSDFString
+from beaker.utils.io import _parseMolData
 from beaker.core_apps.D2Coords.impl import _check3Dcoords
 from chembl_structure_pipeline.standardizer import remove_hs_from_mol
+from chembl_structure_pipeline.standardizer import parse_molblock
 from rdkit import Chem
+import io
+
+
+def _getSDFString(mols):
+    sio = io.StringIO()
+    _create_sdf(sio, mols)
+    return sio.getvalue()
+
+
+def _create_sdf(f, mols):
+    for mol, props in mols:
+        m = Chem.MolToMolBlock(mol)
+        if props:
+            m += props
+        f.write(m + '\n\n$$$$\n')
 
 # ----------------------------------------------------------------------------------------------------------------------
 
-
 def _removeHs(data):
-    mols = _parseMolData(data, loadMol=True, useRDKitChemistry=False)
-    for mol in mols:
+    mols = _parseMolData(data, loadMol=False, useRDKitChemistry=False)
+    ms = []
+    for molblock in mols:
+        mol = parse_molblock(molblock, useRDKitChemistry=False)
+        props = molblock.split("M  END")[1].strip()
+        props =  props if len(props) > 1 else None
         Chem.FastFindRings(mol)
         if mol.NeedsUpdatePropertyCache():
             mol.UpdatePropertyCache(strict=False)
@@ -17,5 +35,5 @@ def _removeHs(data):
             Chem.AssignStereochemistryFrom3D(mol)
         else:
             Chem.AssignAtomChiralTagsFromStructure(mol)
-    mols = _apply(mols, remove_hs_from_mol)
-    return _getSDFString(mols)
+        ms.append((remove_hs_from_mol(mol), props))
+    return _getSDFString(ms)
