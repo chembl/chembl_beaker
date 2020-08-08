8251586cb3154f264e9fd4a8efffa436ffec132a
Eloy Felix <efelix@ebi.ac.uk>
2020-08-08 03:01:45 +0000

    update removehs

--- src/chembl_beaker/beaker/core_apps/calculations/impl.py
+++ src/chembl_beaker/beaker/core_apps/calculations/impl.py
@@ -1,5 +1,4 @@
 from beaker.utils.io import _parseMolData
-from chembl_structure_pipeline.standardizer import remove_hs_from_mol
 from chembl_structure_pipeline.standardizer import parse_molblock
 from rdkit import Chem
 import io
@@ -13,23 +12,38 @@ def _getSDFString(mols):
 
 def _create_sdf(f, mols):
     for mol, props in mols:
-        m = Chem.MolToMolBlock(mol)
+        m = Chem.MolToMolBlock(mol, kekulize=True)
         if props:
             m += props
         f.write(m + '\n$$$$\n')
 
 # ----------------------------------------------------------------------------------------------------------------------
 
+
+def remove_hs_from_mol(m):
+    indices = []
+    for atom in m.GetAtoms():
+        if atom.GetAtomicNum() == 1:
+            bnd = atom.GetBonds()[0]
+            if not (bnd.GetBondDir() in (Chem.BondDir.BEGINWEDGE, Chem.BondDir.BEGINDASH)) and \
+                    not (bnd.HasProp("_MolFileBondStereo") and bnd.GetUnsignedProp("_MolFileBondStereo") in (1, 6)):
+                indices.append(atom.GetIdx())
+
+    mol = Chem.RWMol(m)
+    for index in sorted(indices, reverse=True):
+        mol.RemoveAtom(index)
+    return mol
+
+
 def _removeHs(data):
     mols = _parseMolData(data, loadMol=False, useRDKitChemistry=False)
     ms = []
     for molblock in mols:
-        mol = parse_molblock(molblock, useRDKitChemistry=False)
         props = molblock.split("M  END")[1].strip()
         props =  props if len(props) > 1 else None
+        mol = parse_molblock(molblock, useRDKitChemistry=False)
         Chem.FastFindRings(mol)
         mol.UpdatePropertyCache(strict=False)
-        Chem.AssignAtomChiralTagsFromStructure(mol)
-        Chem.AssignStereochemistry(mol, cleanIt=True, force=True)
-        ms.append((remove_hs_from_mol(mol), props))
+        mol = remove_hs_from_mol(mol)
+        ms.append((mol, props))
     return _getSDFString(ms)
